b7b759e97b2568806087f77d97de0b36743d2571
Younes Id Talab <113123436+younesidt@users.noreply.github.com>
2024-01-30 13:23:50 +0100

    Select City

--- front-end/src/views/SelectCity.vue
+++ front-end/src/views/SelectCity.vue
@@ -1,8 +1,12 @@
 <template>
-    <TaglineComponent/>
-    <StepComponentVue text="1. Dans quelle ville habitez vous ?"/>
-    <DropDownComponent/>
-    <NextMoveComponent StepNum="1" AllSteps="4" NextStep="/" PrivusStep="/packs"/>
+    <div class="w-full container space-y-16">
+        <TaglineComponent/>
+        <div class="space-y-8">
+            <StepComponentVue text="1. Dans quelle ville habitez vous ?"/>
+            <DropDownComponent/>
+        </div>
+        <NextMoveComponent StepNum="1" AllSteps="4" NextStep="/" PrivusStep="/packs"/>
+    </div>
 </template>
 <script setup>
 import TaglineComponent from '../components/shopping/TaglineComponent.vue';
