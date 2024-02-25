a4fd0560bd49f9413b39357dd515b4598ccbb4a9
abderrhim <abderrahim.ait.oufassi2004@gmail.com>
2024-02-25 14:31:08 +0000

    list scolir component

--- front-end/src/components/shopping/NextMoveComponent.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="w-full py-[17px] md:py-[40px] flex flex-row justify-between items-center">
-            <router-link to="/"><button><img class="w-auto md:w-[60px]" src="../../assets/back-arraw.svg"></button></router-link>
-            <router-link to="/prodacts"><button class="px-[60px] flex items-center justify-center py-3 text-white bg-dark-blue rounded-full text-white-color">Suivant</button></router-link>
-        </div>
-</template>
-<script setup>
-</script>
--- front-end/src/views/FrontaireView.vue
+++ front-end/src/views/FrontaireView.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <HeroSection/>
-        <etaps/>
+        <!-- <etaps/> -->
     </div>
     <div class="w-full my-[50px] px-8 absolute">
-        <div class="container m-auto  border rounded md:p-8 p-4">
+        <div class="container m-auto  border-2 border-[#004079]/20 md:rounded-[81px] rounded-3xl  md:p-8 p-4">
             <etapDescription/>
+            <cart></cart>
+            <div class="w-5/6 p-4 md:p-8 flex flex-row justify-between items-start ">
+                <div class="text-[11px] md:text-[21px] font-semibold text-dark-blue ">total</div>
+                <div><button class="text-white-color font-semibold text-[12px] md:text-[20px] px-8 py-2 rounded-full bg-dark-blue">Ajouter au panier (4 articles) </button></div>
+            </div>
         </div>
     </div>
 </template>
@@ -13,4 +18,5 @@
 import HeroSection from '../components/prodacts/heroSectionComponent.vue'
 import etaps from '../components/prodacts/etaps.vue'
 import etapDescription from '../components/prodacts/etapADescriptin.vue'
+import cart from '../components/prodacts/CartComponent.vue'
 </script>
