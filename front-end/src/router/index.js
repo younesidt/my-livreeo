f2f33e0c09f9f2c9f5d846dc2194fe3bd2ab4c77
abderrhim <abderrahim.ait.oufassi2004@gmail.com>
2024-02-26 16:58:58 +0000

    paniare style

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -14,6 +14,7 @@ import Partnerships from '../views/about-views/PartnershipsView.vue'
 import RouterPolicy from '../views/about-views/RouterPolicyView.vue'
 import Livreson from '../views/about-views/LivresonModalitiView.vue'
 import StatutCommande from '../views/StatutCommande.vue'
+import Bascket from '../views/BasketView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
             path: '/pack-selection',
             name: 'pack-selection',
             component: PackSelection
+          },
+          {
+            path: '/bascket',
+            name: ' bascket',
+            component:  Bascket
           },
           {
             path: '/prodacts',
---
+++ front-end/src/views/BasketView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class=" bg-white-blue">
+        <div class="container mx-auto max-w-7xl">
+        <div class="w-full flex flex-col items-center justify-center text-dark-blue">
+            <div class="w-[86%] flex flex-col pt-6 2xl:pt-8">
+                <div>
+                    <img src="../assets/back.svg">
+                </div>
+                <div class="w-full flex flex-col md:flex-row justify-between items-start my-8">
+                    <leftColom/>
+                    <rightColom/>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+<script setup>
+import rightColom from '../components/basket/rightColomComponent.vue'
+import leftColom from '../components/basket/leftColomComponent.vue'
+</script>
